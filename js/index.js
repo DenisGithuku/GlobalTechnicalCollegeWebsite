bf24611524469df394234b12713916c3b3bb4b10
Denis Githuku <githukudenis@gmail.com>
2023-02-02 18:42:23 +0300

    Home page rewrite

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
+/* Get reference to the navbar */
+const navbar = document.querySelector(".main-nav");
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 let prevScrollpos = window.pageYOffset;
+
+/* Get the current navbar scroll offset */
+const navbarOffset = navbar.offsetTop;
+
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".main-nav").style.top = "0";
+  if (prevScrollpos > navbarOffset) {
+    navbar.classList.add("sticky");
   } else {
-    document.querySelector(".main-nav").style.top = "-100px";
+    navbar.classList.remove("sticky");
   }
   prevScrollpos = currentScrollPos;
 };
